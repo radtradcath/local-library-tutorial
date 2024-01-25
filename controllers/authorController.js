19b33834edccc5ac884f5398e6fcc5da70931703
radtradcath <vinicius.b.bonafini@gmail.com>
2024-01-25 16:07:43 -0300

    Create 'Create Book' POST

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,49 +1,102 @@
-const Author = require('../models/author')
-const Book = require('../models/book')
-const asyncHandler = require('express-async-handler')
+const Author = require("../models/author");
+const Book = require("../models/book");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.author_list = asyncHandler(async (req, res, next) => {
-    const allAuthors = await Author.find().sort({family_name: 1}).exec()
+  const allAuthors = await Author.find().sort({ family_name: 1 }).exec();
 
-    res.render('author_list', {title: 'Author List', author_list: allAuthors})
-})
+  res.render("author_list", { title: "Author List", author_list: allAuthors });
+});
 
 exports.author_detail = asyncHandler(async (req, res, next) => {
-    const [author, allBooksByAuthor] = await Promise.all([
-        Author.findById(req.params.id).exec(),
-        Book.find({author: req.params.id}, 'title summary').exec(),
-    ]);
+  const [author, allBooksByAuthor] = await Promise.all([
+    Author.findById(req.params.id).exec(),
+    Book.find({ author: req.params.id }, "title summary").exec(),
+  ]);
 
-    if (author === null) {
-        const err = new Error('Author not found');
-        err.status = 404;
-        return next(err);
-    }
+  if (author === null) {
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
 
-    res.render('author_detail', {title: 'Author Detail', author: author, author_books: allBooksByAuthor})
-})
+  res.render("author_detail", {
+    title: "Author Detail",
+    author: author,
+    author_books: allBooksByAuthor,
+  });
+});
 
-exports.author_create_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create GET')
-})
+exports.author_create_get = asyncHandler((req, res, next) => {
+  res.render("author_form", { title: "Create Author", author: undefined, errors: undefined  });
+});
 
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author create POST')
-})
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+
+    body('family_name')
+        .trim()
+        .isLength({min: 1})
+        .escape()
+        .withMessage('Family name must be specified.')
+        .isAlphanumeric()
+        .withMessage('Family name has non-alphanumeric characters.'),
+
+    body('date_of_birth', 'Invalid date of birth')
+    .optional({values: "falsy"})
+    .isISO8601()
+    .toDate(),
+
+    body('date_of_death', 'Invalid date of death')
+    .optional({values: 'falsy'})
+    .isISO8601()
+    .toDate(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const author = new Author({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+        });
+
+        if (!errors.isEmpty()) {
+            res.render('author_form', {
+                title: "Create Author",
+                author: author,
+                errors: errors.array()
+            });
+            return;
+        } else {
+            await author.save();
+            console.log('saved')
+        }
+
+        res.redirect(author.url)
+    })
+];
 
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author delete GET')
-})
+  res.send("NOT IMPLEMENTED: Author delete GET");
+});
 
 exports.author_delete_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author delete POST')
-})
+  res.send("NOT IMPLEMENTED: Author delete POST");
+});
 
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author update GET')
-})
+  res.send("NOT IMPLEMENTED: Author update GET");
+});
 
 exports.author_update_post = asyncHandler(async (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Author update POST')
-})
-
+  res.send("NOT IMPLEMENTED: Author update POST");
+});
